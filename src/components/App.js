70cf570debcb01498594a0f847fe33c7e0c38736
Your Name <qqgm3rk9k@gmail.com>
2019-09-04 17:30:11 +0900

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import { fab } from '@fortawesome/free-brands-svg-icons'; //fontawesomeのbrand�
 import { fas } from '@fortawesome/free-solid-svg-icons'; //fontawesomeのsolidアイコンのインポート
 import { far } from '@fortawesome/free-regular-svg-icons'; //fontawesomeのregularアイコンのインポート
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Collapse } from 'react-collapse';
 //import { Transition } from 'react-transition-group';
 //import { CSSTransition } from "react-transition-group";
 //import { Collapse } from 'reactstrap';
@@ -14,7 +15,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 library.add(fab, fas, far);
 
 const wrapperStyle = {
-  backgroundColor: "#afeeee",
+  //backgroundColor: "#afeeee",
   //height: '750px',
 }
 const cardStyle = {
@@ -22,40 +23,58 @@ const cardStyle = {
 }
 
 class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = { collapse: false , isOpened: this.props.isOpened};
+  }
+
+  toggle() {
+    this.setState(state => ({ collapse: !state.collapse }));
+  }
+
+
   onSwipeNope() {
     console.log("pushed NopeButton.");
   }
   onSwipeLeft(data) {
     console.log(data.name + " was Leftswiped.");
+    this.setState({ collapse: false });
   }
   onSwipeRight(data){
     console.log(data.name + " was Rightswiped.");
+    this.setState({ collapse: false });
   }
   renderCards() {
     var person = [
                 {
                   id: 1,
-                  name: "taro",
+                  name: "Taro",
                   age: "22",
-                  img: <img src = './taro.png' alt = 'taro' className = 'person-img'/> 
+                  img: <img src = './taro.png' alt = 'taro' className = 'person-img'/>,
+                  detal: "Taro's details screen"
                 },
                 {
                   id: 2,
-                  name: "hanako",
+                  name: "Hanako",
                   age: "21",
-                  img: <img src = './hanako.png' alt = 'hanako' className = 'person-img'/>
+                  img: <img src = './hanako.png' alt = 'hanako' className = 'person-img'/>,
+                  detal: "Hanako's details screen"
                 },
                 {
                   id: 3,
-                  name: "rina",
+                  name: "Rina",
                   age: "23",
-                  img: <img src = './rina.png' alt = 'rina' className = 'person-img'/>
+                  img: <img src = './rina.png' alt = 'rina' className = 'person-img'/>,
+                  detal: "Rina's details screen"
                 },
                 {
                   id: 4,
-                  name: "kenta",
+                  name: "Kenta",
                   age: "25",
-                  img: <img src = './kenta.png' alt = 'kenta' className = 'person-img'/>
+                  img: <img src = './kenta.png' alt = 'kenta' className = 'person-img'/>,
+                  detal: "Kenta's details screen"
                 }
               ];
     return person.map((d) => {
@@ -65,15 +84,17 @@ class App extends React.Component{
         onSwipeLeft={this.onSwipeLeft.bind(this)}
         onSwipeRight={this.onSwipeRight.bind(this)}
         data={d}
-        style={cardStyle}>
-          
+        style={cardStyle}>          
           {d.img}
         <span className = 'font'>
         {d.name}, {d.age}
         </span>
         <div className = 'button-position'>
         <span>
-          <button　className = 'detail-button' onClick = {() => {console.log('detail')}}>Open/Close</button>
+          <button　className = 'detail' onClick = {() => {this.toggle()}}>Open / Close</button>
+          <Collapse isOpened={this.state.collapse}>
+          <div>{d.detal}</div>
+          </Collapse>
         </span>
         </div>
         </Card>
